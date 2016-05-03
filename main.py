f1a2a3c62d05b88d4dbd99dabf0d6884fa1338b5
pparasurama <pparasurama@gmail.com>
2016-05-03 18:16:05 -0400

    keyword search fixed

--- main.py
+++ main.py
@@ -9,18 +9,17 @@ from bokeh.io import curdoc
 # load dataset
 df = pd.read_pickle('shows_df.pkl')
 
-# clean data set
-df.fillna(np.nan, inplace=True)
-
 # get column of list of genre names from list of dicts
 
 
 def get_values(list):
-    values = str([list[i]['name'] for i in range(0,len(list))])
+    values = str([list[i]['name'] for i in range(0,len(list))]).strip("[]")
     return values
 
 df['genre_list'] = df['genres'].map(get_values)
 df['keyword_list'] = df['keywords'].map(get_values)
+
+df.fillna(np.nan, inplace=True)
 
 # controls
 votes = Slider(title="Minimum number of votes", value=0, start=5, end=df['vote_count'].max(), step=10)
@@ -28,7 +27,7 @@ rating = Slider(title="Minimum average rating", value=0.0, start=0.0, end=df['vo
 status = Select(title='Show Status', options=['All', 'Ended', 'Returning Series', 'Canceled', 'In Production', 'Planned'], value='All')
 #show_type = Select(title='Show Type', options=['All', 'Reality', 'Documentary', 'Talk Show', 'Scripted'], value='All')
 genre = Select(title='Genre', options=list(pd.read_pickle('genres.pkl')), value='All')
-keyword = TextInput(title='Keywords')
+keyword = TextInput(title='Keyword')
 
 # initialize data source for bokeh
 source1 = ColumnDataSource(data=dict(x=[], y=[], title=[], ym=[1, 1], x2=[10,600]))
@@ -62,17 +61,28 @@ def subset_shows():
     if genre.value != 'All':
         subset = subset[subset.genre_list.str.contains(genre.value)]
 
-    if keyword.value != '':
-        subset = subset[subset.keyword_list.str.contains(keyword.value) == True]
+    if keyword.value.strip() != '':
+        if any(subset.keyword_list.str.contains(keyword.value.strip())) == False:
+            subset = subset[subset['name'] == 'No Valid Shows']
+        else:
+            subset = subset[subset.keyword_list.str.contains(keyword.value.strip())]
     return subset
+
 
 def update(attrname, old, new):
     df2 = subset_shows()
-    source1.data = dict(x=df2['vote_count'],
-                        y=df2['vote_average'],
-                        title=df2['name'],
-                        ym=[df2['vote_average'].mean(), df2['vote_average'].mean()],
-                        x2=[df2['vote_count'].min()-2,df2['vote_count'].max()])
+    if len(df2) > 0:
+        source1.data = dict(x=df2['vote_count'],
+                            y=df2['vote_average'],
+                            title=df2['name'],
+                            ym=[df2['vote_average'].mean(), df2['vote_average'].mean()],
+                            x2=[df2['vote_count'].min()-2,df2['vote_count'].max()])
+    else:
+        source1.data = dict(x=df2['vote_count'],
+                            y=df2['vote_average'],
+                            title=df2['name'],
+                            ym=[0,0],
+                            x2=[0, 0])
 
 controls = [votes, rating, status, genre, keyword]
 for control in controls:
